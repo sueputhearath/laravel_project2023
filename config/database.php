adc352594412b63357a340fddf70c63241658563
sueputhearath <sueputhearath232@gmail.com>
2023-08-21 00:29:32 +0700

    d

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
         ],
 
         'mysql' => [
-            'driver' => 'pgsql',
+            'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
@@ -69,7 +69,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'ecommerce2023'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
